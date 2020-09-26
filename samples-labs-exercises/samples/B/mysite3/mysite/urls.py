1d8de8658923f0820d55d864db361671dce3aebb
Justin Lin <caterpillar@openhome.cc>
2020-09-26 10:13:22 +0800

    update samples

--- samples-labs-exercises/samples/B/mysite3/mysite/urls.py
+++ samples-labs-exercises/samples/B/mysite3/mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
-
--- samples-labs-exercises/samples/B/mysite3/polls/urls.py
+++ samples-labs-exercises/samples/B/mysite3/polls/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
